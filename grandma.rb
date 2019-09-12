6f8fd600ca5d3e223d7888013ea052caafb36183
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-12 14:47:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,26 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+  
+  # If phrase is "I love you," then...
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    
+  # If phrase is ALL UPPERCASE
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+    
+  # If phrase IS all uppercase, then...
+  elsif
+    return "NO, NOT SINCE 1938!"
+  
+  #
+  elsif
+    
+
+  end
+
+
+end
